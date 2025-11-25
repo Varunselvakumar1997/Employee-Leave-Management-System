d6d9b77e76ea82815609ebe2d05a6a141b431d66
varun <varun.s@devopslabs.tech>
2025-11-25 16:55:48 +0530

    Added notification service

---
+++ employeeNotificationService/src/main/java/tech/employeeNotificationService/employeeNotificationService/EmployeeNotificationServiceApplication.java
@@ -0,0 +1,27 @@
+package tech.employeeNotificationService.employeeNotificationService;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class EmployeeNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeNotificationServiceApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		return mapper;
+	}
+
+}
