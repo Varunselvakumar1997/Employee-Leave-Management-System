df9b5bab4480f63ff993dd40b9d206ac39aafd36
varun <varun.s@devopslabs.tech>
2025-11-27 20:33:36 +0530

    Added notification service

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM azul/zulu-openjdk-alpine:21-jre-headless
-
-WORKDIR /app
-
-COPY build/libs/employee-leave-management-system-0.0.1-SNAPSHOT.jar app.jar
-
-EXPOSE 8080
-
-ENTRYPOINT ["java","-jar","app.jar"]
---
+++ notification-service/src/test/java/tech/employeeNotificationService/employeeNotificationService/EmployeeNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.employeeNotificationService.employeeNotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
